b222833f9e7b04ec46a2a52c24d9d20779d4153d
antonio.martin <antonio.martin@schibsted.com>
2020-03-18 09:30:04 +0100

    004-setting title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: 'description',
-        name: 'description',
+        hid: 'estrenoscinehoy',
+        name: 'estrenos cine hoy',
         content: process.env.npm_package_description || ''
       }
     ],
