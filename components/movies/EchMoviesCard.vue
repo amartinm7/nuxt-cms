33fdcec8cadae89726d6fcc20fc6046bca0b3039
antonio.martin <antonio.martin@schibsted.com>
2020-05-13 22:32:37 +0200

    fixing video another time, without header

--- components/movies/EchMoviesCard.vue
+++ components/movies/EchMoviesCard.vue
@@ -12,7 +12,6 @@
           &nbsp;{{ movie._vote_average.toFixed(1) }}&nbsp;
         </span>
         <a
-          href="#modalcenter"
           class="uk-button uk-button-default uk-align-center"
           @click="initVideoURL(movie)"
         >
