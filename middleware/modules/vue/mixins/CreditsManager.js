b9e2f0b80291a79101463cec26a169334b124351
antonio.martin <antonio.martin@schibsted.com>
2020-05-26 23:00:41 +0200

    setup number of items by default

--- middleware/modules/vue/mixins/CreditsManager.js
+++ middleware/modules/vue/mixins/CreditsManager.js
@@ -4,17 +4,21 @@ export default {
       const credits = this.movies[0]._credits.filter(
         (credit) => !!credit.profile_path
       )
-      return credits < 6 ? credits.concat(credits) : credits
+      return credits.length < 6
+        ? credits.concat(credits).concat(credits)
+        : credits
     },
     posters() {
       const posters = this.movies[0]._images._posters
         .filter((poster) => !!poster._file_path)
         .filter((poster) => poster._vote_count > 1)
-      return posters < 6 ? posters.concat(posters) : posters
+      return posters.length < 6
+        ? posters.concat(posters).concat(posters)
+        : posters
     },
     videos() {
       const videos = this.movies[0]._videos
-      return videos.length < 6 ? videos.concat(videos) : videos
+      return videos.length < 6 ? videos.concat(videos).concat(videos) : videos
     }
   }
 }
