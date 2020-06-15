c9d942e76e0258005d4f42b0ce415453d5aac309
antonio.martin <antonio.martin@schibsted.com>
2020-06-15 23:55:06 +0200

    adding preview for social media networks

--- middleware/modules/vue/mixins/DetailsHeaderManager.js
+++ middleware/modules/vue/mixins/DetailsHeaderManager.js
@@ -1,42 +1,42 @@
-// import MediaHandler from '../../../framework/modules/media/MediaHandler'
+import MediaHandler from '../../../framework/modules/media/MediaHandler'
 import StringHandler from '../../../framework/modules/string/StringHandler'
 
 export default {
   head() {
     const vm = this
+    const title = vm.movies[0]._title || vm.movies[0]._name
+    const keyword =
+      'Estrenos cine hoy, peliculas, series, actores, actrices, TV Shows, movies, actor, actress, movies rating, news, tv networks'
     return {
-      title: `Estrenos Cine Hoy: ${vm.movies[0]._title || vm.movies[0]._name}`,
+      title: `${StringHandler.truncate(title, 65)}`,
       meta: [
-        {
-          hid: 'keywords',
-          name: 'keywords',
-          content:
-            'Estrenos cine hoy, peliculas, series, actores, actrices, TV Shows, movies, actor, actress, movies rating, news, tv networks'
-        },
-        {
-          hid: 'description',
-          name: 'description',
-          content: `${StringHandler.truncate(vm.movies[0]._overview, 100)}...`
-        },
         {
           charset: 'utf-8'
         },
         {
+          hid: 'keywords',
+          name: 'keywords',
+          content: keyword
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: `${StringHandler.truncate(vm.movies[0]._overview, 155)}`
+        },
+        {
           hid: 'og:title',
           property: 'og:title',
-          content: `Estrenos Cine Hoy: ${vm.movies[0]._title ||
-            vm.movies[0]._name}`
+          content: `${StringHandler.truncate(title, 35)}`
         },
         {
           hid: 'og:description',
           name: 'og:description',
-          content: `${StringHandler.truncate(vm.movies[0]._overview, 100)}...`
+          content: `${StringHandler.truncate(vm.movies[0]._overview, 65)}`
         },
         {
           hid: 'og:image',
           property: 'og:image',
-          // content: MediaHandler.getPosterURL(vm.movies[0]._poster_path)
-          content: 'https://www.computerhope.com/jargon/j/jpg.jpg'
+          content: MediaHandler.getPosterURL(vm.movies[0]._poster_path)
         },
         {
           hid: 'og:url',
