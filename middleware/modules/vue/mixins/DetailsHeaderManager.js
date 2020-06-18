9fd25757b15402bf029689529171acf2b087c410
antonio.martin <antonio.martin@schibsted.com>
2020-06-18 13:01:48 +0200

    adding og tags for tweeter

--- middleware/modules/vue/mixins/DetailsHeaderManager.js
+++ middleware/modules/vue/mixins/DetailsHeaderManager.js
@@ -9,9 +9,15 @@ export default {
       'Estrenos cine hoy, peliculas, series, actores, actrices, TV Shows, movies, actor, actress, movies rating, news, tv networks'
     return {
       title: `${StringHandler.truncate(title, 65)}`,
+      description: keyword,
       meta: [
         {
           charset: 'utf-8'
+        },
+        {
+          hid: 'title',
+          name: 'title',
+          content: title
         },
         {
           hid: 'keywords',
@@ -39,6 +45,36 @@ export default {
           content: MediaHandler.getPosterURL(vm.movies[0]._poster_path)
         },
         {
+          hid: 'twitter:title',
+          property: 'og:title',
+          content: `${StringHandler.truncate(title, 35)}`
+        },
+        {
+          hid: 'twitter:description',
+          name: 'og:description',
+          content: `${StringHandler.truncate(vm.movies[0]._overview, 65)}`
+        },
+        {
+          hid: 'twitter:image',
+          property: 'twitter:image',
+          content: MediaHandler.getPosterURL(vm.movies[0]._poster_path)
+        },
+        {
+          hid: 'twitter:url',
+          property: 'twitter:url',
+          content: `www.estrenoscinehoy.com/${vm.$route.path}`
+        },
+        {
+          hid: 'twitter:card',
+          property: 'twitter:card',
+          content: MediaHandler.getPosterURL(vm.movies[0]._poster_path)
+        },
+        {
+          hid: 'og:type',
+          property: 'og:type',
+          content: 'website'
+        },
+        {
           hid: 'og:url',
           property: 'og:url',
           content: `www.estrenoscinehoy.com/${vm.$route.path}`
@@ -62,3 +98,32 @@ export default {
     }
   }
 }
+/**
+<!-- Primary Meta Tags -->
+<title>estrenoscinehoy</title>
+<meta name="title" content="estrenoscinehoy">
+  <meta name="description" content="dfdsfdfdsfdsfdsffddfdsf dsf
+
+sdfadsfdf
+sdfdsfdsf">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+  <meta property="og:url" content="https://www.estrenoscinehoy.com/es/movie/details/475430-artemis-fowl">
+  <meta property="og:title" content="estrenoscinehoy">
+  <meta property="og:description" content="dfdsfdfdsfdsfdsffddfdsf dsf
+
+sdfadsfdf
+sdfdsfdsf">
+<meta property="og:image" content="">
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://www.estrenoscinehoy.com/es/movie/details/475430-artemis-fowl">
+  <meta property="twitter:title" content="estrenoscinehoy">
+  <meta property="twitter:description" content="dfdsfdfdsfdsfdsffddfdsf dsf
+
+sdfadsfdf
+sdfdsfdsf">
+<meta property="twitter:image" content="">
+**/
