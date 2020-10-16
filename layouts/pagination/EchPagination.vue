b744deb4ed8682cac01ec9df2f344ff59f26f6c3
antonio.martin <antonio.martin@schibsted.com>
2020-10-17 01:23:45 +0200

    setting up margins to plus and minus buttons

--- layouts/pagination/EchPagination.vue
+++ layouts/pagination/EchPagination.vue
@@ -2,14 +2,14 @@
   <div>
     <div class="uk-flex uk-flex-between ech-scrollspy-effect">
       <span
-        class="uk-margin-small-right uk-icon ech-spin-icon"
+        class="uk-margin-small-right uk-icon ech-spin-icon uk-margin-small-top"
         uk-icon="icon: minus;"
         :uk-tooltip="$t('previousResults')"
         @click="toPrevious"
       ></span>
       <span></span>
       <span
-        class="uk-margin-small-left uk-icon ech-spin-icon"
+        class="uk-margin-small-left uk-icon ech-spin-icon uk-margin-small-top"
         uk-icon="icon: plus;"
         :uk-tooltip="$t('nextResults')"
         @click="toNext"
