da66bc51a60a02fbb40234b173afe5cd9dcdfb5c
antonio.martin <antonio.martin@schibsted.com>
2020-10-18 00:12:58 +0200

    fixing default flash url to play

--- middleware/modules/domain/ValuesByDefault.js
+++ middleware/modules/domain/ValuesByDefault.js
@@ -1,6 +1,5 @@
 export default {
   posterPath: 'wHa6KOJAoNTFLFtp7wguUJKSnju.jpg',
-  videoURL:
-    'https://www.youtube.com/embed/Yj0l7iGKh8g?autoplay=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=1',
+  videoURL: '',
   logoPath: 'wwemzKWzjKYJFfCeiB57q3r4Bcm.png'
 }
--- middleware/modules/vue/mixins/VideoControllerManager.js
+++ middleware/modules/vue/mixins/VideoControllerManager.js
@@ -1,22 +1,20 @@
-import ValuesByDefault from '../../domain/ValuesByDefault'
 export default {
   data() {
     return {
-      url:
-        'https://www.youtube.com/embed/Yj0l7iGKh8g?autoplay=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=1',
+      url: '',
       showVideo: false
     }
   },
   methods: {
     playVideoURL(url) {
-      this.url = url ?? ValuesByDefault.videoURL
+      this.url = url ?? ''
       this.$uikit.modal('#modalcenter').show()
     },
     clearVideoURL() {
       this.url = ''
     },
     playVideo(url) {
-      this.url = url ?? ValuesByDefault.videoURL
+      this.url = url ?? ''
       this.showVideo = true
     },
     closeVideo() {
