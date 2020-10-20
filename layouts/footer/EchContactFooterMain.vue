a30105233727b82e8b67d89d5f1d4d16698a9b56
antonio.martin <antonio.martin@schibsted.com>
2020-10-20 22:03:41 +0200

    hidden removing yootheme stay tuned section

--- layouts/footer/EchContactFooterMain.vue
+++ layouts/footer/EchContactFooterMain.vue
@@ -1,17 +1,8 @@
 <template>
   <div id="tm-bottom" class="uk-section-image">
     <div
-      data-src="https://getuikit.com/pro/themes/wordpress/2017/joline/wp-content/themes/yootheme/cache/home-bottom-0d42a025.webp"
-      data-srcset="https://getuikit.com/pro/themes/wordpress/2017/joline/wp-content/themes/yootheme/cache/home-bottom-47fb92c8.webp 768w,
-      https://getuikit.com/pro/themes/wordpress/2017/joline/wp-admin/admin-ajax.php?action=kernel&amp;p=theme%2Fimage&amp;src=WyJ3cC1jb250ZW50XC91cGxvYWRzXC9ob21lLWJvdHRvbS5qcGciLFtbImRvUmVzaXplIixbMTAyNSwzMjgsMTAyNSwzMjhdXSxbImRvQ3JvcCIsWzEwMjQsMzI4LDAsMF1dLFsidHlwZSIsWyJ3ZWJwIiwiODUiXV1dXQ%3D%3D&amp;hash=2c487522bedcb2359678643de4307911 1024w,
-      https://getuikit.com/pro/themes/wordpress/2017/joline/wp-content/themes/yootheme/cache/home-bottom-3547e7ae.webp 1366w,
-      https://getuikit.com/pro/themes/wordpress/2017/joline/wp-content/themes/yootheme/cache/home-bottom-3038b43e.webp 1600w,
-      https://getuikit.com/pro/themes/wordpress/2017/joline/wp-content/themes/yootheme/cache/home-bottom-ce4c4bf5.webp 1920w,
-      https://getuikit.com/pro/themes/wordpress/2017/joline/wp-content/themes/yootheme/cache/home-bottom-0d42a025.webp 3000w"
-      data-sizes="(max-aspect-ratio: 3000/960) 313vh"
-      uk-img=""
       class="uk-background-norepeat uk-background-cover uk-background-top-right uk-section uk-section-large"
-      style="background-color: rgb(255, 255, 255); background-image: url('https://demo.yootheme.com/themes/wordpress/2017/joline/wp-content/themes/yootheme/cache/home-bottom-0d42a025.webp');"
+      style="background-color: rgb(255, 255, 255); "
     >
       <div class="uk-container uk-container-expand">
         <div class="uk-child-width-expand@m uk-grid uk-grid-stack" uk-grid="">
