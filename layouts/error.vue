01c111d975ff1a2bf99be00fe1c810ca4787556d
antonio.martin <antonio.martin@schibsted.com>
2020-10-23 20:09:44 +0200

    adding default error page. we can improve it in the futture with a 301 redirect to the index page

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,7 +4,11 @@
       <ech-header-main @outbound-open-video-modal="playVideo"></ech-header-main>
     </section>
     <section class="uk-section uk-section-xsmall">
-      <nuxt-link :to="getHomeURL()" class="uk-link-reset">
+      <nuxt-link
+        :to="getHomeURL()"
+        class="uk-link-reset"
+        uk-tooltip="click here!"
+      >
         <h1 class="uk-text-center">Estrns cine hoy</h1>
       </nuxt-link>
     </section>
