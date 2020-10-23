cedddfac41f1beb9ce58c1673e206f20bb87ac20
antonio.martin <antonio.martin@schibsted.com>
2020-10-23 11:37:06 +0200

    adding defaut logo ech when image is empty

--- middleware/framework/modules/media/MediaHandler.js
+++ middleware/framework/modules/media/MediaHandler.js
@@ -2,12 +2,14 @@ import ValuesByDefault from '../../../modules/domain/ValuesByDefault'
 
 export default class MediaHandler {
   static getPosterURL(posterPath) {
-    const sanitizedPosterPath = posterPath ?? ValuesByDefault.posterPath
-    return `https://image.tmdb.org/t/p/w185_and_h278_bestv2${sanitizedPosterPath}`
+    return posterPath
+      ? `https://image.tmdb.org/t/p/w185_and_h278_bestv2${posterPath}`
+      : ValuesByDefault.logoURLEstrenosCineHoy
   }
 
   static getPoster2XURL(posterPath) {
-    const sanitizedPosterPath = posterPath ?? ValuesByDefault.posterPath
-    return `https://image.tmdb.org/t/p/w370_and_h556_bestv2${sanitizedPosterPath}`
+    return posterPath
+      ? `https://image.tmdb.org/t/p/w370_and_h556_bestv2${posterPath}`
+      : ValuesByDefault.logoURLEstrenosCineHoy
   }
 }
--- middleware/modules/domain/ValuesByDefault.js
+++ middleware/modules/domain/ValuesByDefault.js
@@ -1,5 +1,7 @@
 export default {
-  posterPath: 'wHa6KOJAoNTFLFtp7wguUJKSnju.jpg',
+  posterPath: '/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg',
   videoURL: '',
-  logoPath: 'wwemzKWzjKYJFfCeiB57q3r4Bcm.png'
+  logoPath: '/wwemzKWzjKYJFfCeiB57q3r4Bcm.png',
+  logoURLEstrenosCineHoy:
+    'https://www.estrenoscinehoy.com/_nuxt/img/isolated-monochrome-black.9dc380f.svg'
 }
