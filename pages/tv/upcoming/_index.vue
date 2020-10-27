1535d7ea6912b2a1a3c754a414f6c90b5c190bf3
antonio.martin <antonio.martin@schibsted.com>
2020-10-27 21:39:16 +0100

    refactoring for trending

---
+++ pages/tv/upcoming/_index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <section v-if="false" class="uk-section uk-section-xsmall">
+      <ech-header-main @outbound-open-video-modal="playVideo"></ech-header-main>
+    </section>
+    <section class="uk-section uk-section-xsmall">
+      <ech-networks-nav-bar></ech-networks-nav-bar>
+      <ech-slider-main :movies="trendingShows._results"> </ech-slider-main>
+      <ech-pagination
+        @outbound-to-previous-page="toPrevious"
+        @outbound-to-next-page="toNext"
+      ></ech-pagination>
+    </section>
+    <section class="uk-section uk-section-xsmall">
+      <h1 class="uk-text-center">{{ getSection(mediaType) }}</h1>
+    </section>
+    <section class="uk-section uk-section-xsmall">
+      <div>
+        <ech-tv-show-card
+          :movies="trendingShows._results"
+          @outbound-open-video-modal="playVideo"
+        ></ech-tv-show-card>
+      </div>
+    </section>
+    <div>
+      <vk-modal center :show.sync="showVideo">
+        <vk-modal-close @click="closeVideo"></vk-modal-close>
+        <iframe
+          :src="url"
+          width="1920"
+          height="1080"
+          frameborder="0"
+          uk-responsive
+          uk-video="automute: true"
+        ></iframe>
+      </vk-modal>
+    </div>
+  </div>
+</template>
+<!-- eslint-disable -->
+<!-- eslint-enable -->
+<script>
+/* eslint-disable camelcase, no-console */
+import EchTvShowCard from '@/components/movies/EchTvShowCard'
+import EchSliderMain from '@/components/slider/EchSliderMain'
+import EchHeaderMain from '@/layouts/header/EchHeaderMain'
+import VideoControllerManager from '@/middleware/modules/vue/mixins/VideoControllerManager'
+import UpcomingManager from '@/middleware/modules/vue/mixins/UpcomingManager'
+import MediaTypes from '@/middleware/modules/domain/MediaTypes'
+import DetailsHeaderManager from '@/middleware/modules/vue/mixins/DetailsHeaderManager'
+import PaginationForTrendingManager from '@/middleware/modules/vue/mixins/PaginationForTrendingManager'
+import EchPagination from '@/layouts/pagination/EchPagination'
+import EchNetworksNavBar from '@/layouts/networksbar/EchNetworksNavBar'
+
+export default {
+  name: 'EchTvshowUpcoming',
+  components: {
+    EchNetworksNavBar,
+    EchPagination,
+    EchHeaderMain,
+    EchSliderMain,
+    EchTvShowCard
+  },
+  mixins: [
+    VideoControllerManager,
+    UpcomingManager,
+    DetailsHeaderManager,
+    PaginationForTrendingManager(MediaTypes.tv)
+  ],
+  data() {
+    return {
+      trendingShows: {
+        _page: 1,
+        _total_pages: 1,
+        _total_results: 1,
+        _results: []
+      },
+      mediaType: MediaTypes.tv,
+      action: '',
+      page: 1
+    }
+  }
+}
+</script>
+
+<style></style>
