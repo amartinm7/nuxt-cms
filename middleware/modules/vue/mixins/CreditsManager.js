d1853805d5f5a0cbc606247cfca8879f5b026aa6
antonio.martin <antonio.martin@schibsted.com>
2020-11-04 21:57:15 +0100

    refill sliders to show them full of pictures

--- middleware/modules/vue/mixins/CreditsManager.js
+++ middleware/modules/vue/mixins/CreditsManager.js
@@ -4,17 +4,33 @@ export default {
   computed: {
     credits() {
       if (_isEmpty(this.movies[0]._credits)) return []
-      return this.movies[0]._credits.filter((credit) => !!credit.profile_path)
+      return this.refillArray(
+        this.movies[0]._credits.filter((credit) => !!credit.profile_path)
+      )
     },
+
     posters() {
       if (_isEmpty(this.movies[0]._images)) return []
       const posters = this.movies[0]._images._posters ?? []
-      return posters
-        .filter((poster) => !!poster._file_path)
-        .filter((poster) => poster._vote_count > 1)
+      return this.refillArray(
+        posters
+          .filter((poster) => !!poster._file_path)
+          .filter((poster) => poster._vote_count > 1)
+      )
     },
     videos() {
-      return this.movies[0]._videos
+      return this.refillArray(this.movies[0]._videos)
+    }
+  },
+  methods: {
+    refillArray(_array) {
+      if (_isEmpty(_array)) return []
+      let recopiedArray = []
+      const rate = 10 / _array.length
+      for (let index = 0; index < rate; index++) {
+        recopiedArray = recopiedArray.concat(_array)
+      }
+      return recopiedArray
     }
   }
 }
