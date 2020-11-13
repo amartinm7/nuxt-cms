e64fbbd8b3258ae6cbe7e4447164fbf20877d0b8
antonio.martin <antonio.martin@schibsted.com>
2020-11-13 23:47:47 +0100

    fixing zero search, redirect to home as default action

--- middleware/modules/vue/mixins/RedirectHomeManager.js
+++ middleware/modules/vue/mixins/RedirectHomeManager.js
@@ -5,7 +5,7 @@ export default {
     const search = this.trendingShows || this.multiSearch
     if (
       _isEmpty(search) ||
-      _isEmpty(search[0]) ||
+      search._results?.length === 0 ||
       search._total_results === 0
     ) {
       const language = this.$i18n.locale
