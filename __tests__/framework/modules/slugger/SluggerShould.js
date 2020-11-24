20722ce55d1ebe1bd94f544858b204de54279be5
antonio.martin <antonio.martin@schibsted.com>
2020-11-24 23:47:00 +0100

    fixing tests

--- __tests__/framework/modules/slugger/SluggerShould.js
+++ __tests__/framework/modules/slugger/SluggerShould.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/named
 import Slugger from '../../../../middleware/framework/modules/slugger/Slugger'
 
+// eslint-disable-next-line no-unused-vars
 const assert = require('assert')
 
 console.log('welcome! SluggerShould test')
@@ -21,7 +22,7 @@ describe('SluggerShould', function() {
       expect(actualSlug).toEqual(expectedSlug)
     })
     it('should slugger two params and &', function() {
-      const expectedSlug = '88396-falcon-&-el-soldado-de-invierno'
+      const expectedSlug = '88396-falcon-el-soldado-de-invierno'
       const movie_id = 88396
       const movie_title = 'Falcon & El Soldado de Invierno'
       const actualSlug = Slugger.sluggify([movie_id, movie_title])
