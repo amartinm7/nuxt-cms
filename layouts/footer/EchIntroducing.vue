8ca197b326c26823f8e01ff22d036a02c8ab5f02
antonio.martin <antonio.martin@schibsted.com>
2020-11-25 22:39:33 +0100

    fixing friend network padding

--- layouts/footer/EchIntroducing.vue
+++ layouts/footer/EchIntroducing.vue
@@ -6,7 +6,7 @@
       <div class="uk-padding-large">
         <div>
           <h2 class="ech-basic uk-text-center">
-            About Us!
+            About Us! Hello ;)
           </h2>
         </div>
         <div
@@ -14,12 +14,36 @@
         >
           <p>
             Estamos construyendo un sitio nuevo para mostrar toda la información
-            sobre series y películas: netflix, hbo, amazon prime, estrenos de
-            las ultimas peliculas en cine, etc. Entre otras, se puede consultar
-            los detalles de tus series favoritas, como su ficha técnica, videos
-            relacionados, reparto actores, y mas... y lo mismo para peliculas...
-            no te pierdas los trailers de las ultimas peliculas de estreno... :)
-            Visita nuestra pagina para apoyarnos y dinos que te parece!
+            sobre series y películas: las mejores series netflix, hbo, amazon
+            prime, estrenos de cine, las mejores películas, cartelera actual
+            etc.
+          </p>
+          <p>
+            Entre otras cosas, en esta web puedes consultar los detalles de tús
+            series favoritas, como su ficha técnica, videos relacionados,
+            reparto actores, episodios por temporada, reviews de las series y
+            peliculas... segun el idioma que selecciones... y mas...
+          </p>
+          <p>
+            ...y lo mismo para peliculas...
+          </p>
+          <p>
+            no te pierdas los trailers de las últimas peliculas de estreno...
+            clicka en las imagenes con el simbolo del play para verlo!
+          </p>
+          <p>
+            y navegando y navegando, usando los enlaces de las páginas, puedes
+            descubrir series y peliculas que seguro te gustan...
+          </p>
+          <p>
+            tambien puedes buscar tu serie o pelicula favorita pulsando el icono
+            de la lupa e introduciendo el titulo... :)
+          </p>
+          <p>
+            Visita nuestra página para apoyarnos y dinos que te parece!
+          </p>
+          <p>
+            Muchas gracias por tu apoyo :)
           </p>
         </div>
       </div>
