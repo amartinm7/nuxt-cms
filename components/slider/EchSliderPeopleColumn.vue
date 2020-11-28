f1ab820cdc77f5bfde387d7d535f1a6a0f5fce9b
antonio.martin <antonio.martin@schibsted.com>
2020-11-28 22:38:17 +0100

    fixing slider columns at the card

--- components/slider/EchSliderPeopleColumn.vue
+++ components/slider/EchSliderPeopleColumn.vue
@@ -1,67 +1,69 @@
 <template>
-  <div class="uk-padding-small">
-    <h2 v-if="isCredits" class="uk-text-lead uk-text-center ech-basic">
-      {{ $t('creditsBy', { name: movieName }) }}
-    </h2>
-    <h2 v-if="isCrew" class="uk-text-lead uk-text-center ech-basic">
-      {{ $t('staffBy', { name: movieName }) }}
-    </h2>
-    <div
-      class="uk-position-relative uk-visible-toggle uk-light"
-      tabindex="-1"
-      uk-slider="clsActivated: uk-transition-active; center: true; autoplay: true;"
-    >
-      <ul
-        class="uk-slider-items"
-        style="transform: translate3d(0px, 0px, 0px);"
+  <div>
+    <div class="uk-card-body">
+      <h2 v-if="isCredits" class="uk-text-lead uk-text-center ech-basic">
+        {{ $t('creditsBy', { name: movieName }) }}
+      </h2>
+      <h2 v-if="isCrew" class="uk-text-lead uk-text-center ech-basic">
+        {{ $t('staffBy', { name: movieName }) }}
+      </h2>
+      <div
+        class="uk-position-relative uk-visible-toggle uk-light"
+        tabindex="-1"
+        uk-slider="clsActivated: uk-transition-active; center: true; autoplay: true;"
       >
-        <a name="echSliderPeople"></a>
-        <li
-          v-for="credit in credits"
-          :key="credit._credit_id + Math.random()"
-          :todo="credit"
+        <ul
+          class="uk-slider-items"
+          style="transform: translate3d(0px, 0px, 0px);"
         >
-          <nuxt-link
-            class="uk-link-reset"
-            :to="getPeoplePathURL(credit._id, credit._name)"
+          <a name="echSliderPeople"></a>
+          <li
+            v-for="credit in credits"
+            :key="credit._credit_id + Math.random()"
+            :todo="credit"
           >
-            <div class="uk-panel">
-              <img
-                :src="getPosterURL(credit._profile_path)"
-                :alt="credit._name"
-                :title="credit._name"
-                :uk-tooltip="credit._name"
-                loading="lazy"
-              />
-              <div
-                class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom"
-              >
-                <p class="uk-margin-remove uk-text-small">
-                  {{ credit._name }}
-                </p>
-                <p v-if="isCredits" class="uk-margin-remove uk-text-small">
-                  {{ $t('as') }} {{ credit._character }}
-                </p>
-                <p v-if="isCrew" class="uk-margin-remove uk-text-small">
-                  {{ $t('as') }} {{ credit._job }}
-                </p>
+            <nuxt-link
+              class="uk-link-reset"
+              :to="getPeoplePathURL(credit._id, credit._name)"
+            >
+              <div class="uk-panel">
+                <img
+                  :src="getPosterURL(credit._profile_path)"
+                  :alt="credit._name"
+                  :title="credit._name"
+                  :uk-tooltip="credit._name"
+                  loading="lazy"
+                />
+                <div
+                  class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom"
+                >
+                  <p class="uk-margin-remove uk-text-small">
+                    {{ credit._name }}
+                  </p>
+                  <p v-if="isCredits" class="uk-margin-remove uk-text-small">
+                    {{ $t('as') }} {{ credit._character }}
+                  </p>
+                  <p v-if="isCrew" class="uk-margin-remove uk-text-small">
+                    {{ $t('as') }} {{ credit._job }}
+                  </p>
+                </div>
               </div>
-            </div>
-          </nuxt-link>
-        </li>
-      </ul>
-      <a
-        class="uk-position-center-left uk-position-small uk-hidden-hover"
-        href="#"
-        uk-slidenav-previous
-        uk-slider-item="previous"
-      ></a>
-      <a
-        class="uk-position-center-right uk-position-small uk-hidden-hover"
-        href="#"
-        uk-slidenav-next
-        uk-slider-item="next"
-      ></a>
+            </nuxt-link>
+          </li>
+        </ul>
+        <a
+          class="uk-position-center-left uk-position-small uk-hidden-hover"
+          href="#"
+          uk-slidenav-previous
+          uk-slider-item="previous"
+        ></a>
+        <a
+          class="uk-position-center-right uk-position-small uk-hidden-hover"
+          href="#"
+          uk-slidenav-next
+          uk-slider-item="next"
+        ></a>
+      </div>
     </div>
   </div>
 </template>
